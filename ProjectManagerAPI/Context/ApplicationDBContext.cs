f37d9e3ab8a0036b6eed9dbf814c424e672aa9b5
Subrato_Sarke <supersubrato@gmail.com>
2024-07-20 17:58:03 +0600

    Show users

--- ProjectManagerAPI/Context/ApplicationDBContext.cs
+++ ProjectManagerAPI/Context/ApplicationDBContext.cs
@@ -10,6 +10,7 @@ namespace ProjectManagerAPI.Context
         public DbSet<APIValidationResponse> APIValidationResponse { get; set; }
         public DbSet<LogInResponse> LogInResponse { get; set; }
         public DbSet<Projects> Projects { get; set; }
+        public DbSet<AllUser> AllUser { get; set; }
         public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
         {
         }
---
+++ ProjectManagerAPI/Controllers/Admin/AdminController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using ProjectManagerAPI.Context;
+using ProjectManagerAPI.Model.ProjectModel;
+using ProjectManagerAPI.Model.User;
+using ProjectManagerAPI.Repository.Security;
+using System.Data;
+using System.Numerics;
+
+namespace ProjectManagerAPI.Controllers.Admin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public AdminController(ApplicationDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [Route("GetAllUser")]
+        [AllowAnonymous]
+        public async Task<List<AllUser>> GetAllUser()
+        {
+            try
+            {
+                var commandText = $"EXEC sprGetAllUser ";
+                var result = _dbContext.AllUser.FromSqlRaw(commandText).ToList();
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                // Handle other exceptions
+                return null;
+            }
+        }
+    }
+}
